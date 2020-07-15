b722b2f59daa9f05b5ae25499716086be2c1ff07
Quinn Krug <quinn.krug@gmail.com>
2020-07-15 14:29:47 -0600

    make employee details dynamic

--- client/components/employee_detail.js
+++ client/components/employee_detail.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const EmployeeDetail = (props) => {
+const EmployeeDetail = ({employee}) => {
+  const {name, email, phone, avatar} = employee;
+  //above line does all 4 below lines. We dont have to use props because of the {employee}
+  // const name = employee.name
+  // const email = employee.email
+  // const phone = employee.phone
+  // const avatar = employee.avatar
+
   return (
-    <div>
-      Employee Detail
+    <div className="thumbnail">
+      <img src={avatar} />
+      <div className="caption">
+        <h3>{name}</h3>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
+        </ul>
+      </div>
     </div>
   )
 };
