859cb271704560620beb5533c9ce65735b96e18b
Quinn Krug <quinn.krug@gmail.com>
2020-07-15 09:41:49 -0600

    set up EMployee List

--- server/main.js
+++ server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
+import _ from 'lodash';
+import {Employees} from '../imports/collections/employees';
+import {image, helpers} from 'faker';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  const numberRecords = Employees.find({}).count();
+  console.log(numberRecords);
+  if (!numberRecords){
+    _.times(5000,() =>{
+      const {name, email, phone} = helpers.createCard();
+
+      Employees.insert({
+        name: name,
+        email: email,
+        phone: phone,
+        avatar: image.avatar()
+      });
+    });
+  }
 });
