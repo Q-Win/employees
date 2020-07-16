ad9164eff36a7b26bf28bdf9160c90a2288da3ae
Quinn Krug <quinn.krug@gmail.com>
2020-07-16 10:16:10 -0600

    Load More button to load more employees

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -6,6 +6,14 @@ import EmployeeDetail from './employee_detail';
 const PER_PAGE = 20;
 
 class EmployeeList extends Component {
+  componentDidMount(){
+    this.page =1;
+  }
+
+  handleButtonClick(){
+    Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+    this.page +=1;
+  }
 
   render() {
     return (
@@ -14,7 +22,7 @@ class EmployeeList extends Component {
           {this.props.employees.map(employee =>
             <EmployeeDetail key={employee._id} employee={employee} />)}
         </div>
-        <button onClick={() => Meteor.subscribe('employees', 40)} className="btn btn-primary"> Load More</button>
+        <button onClick={this.handleButtonClick.bind(this) } className="btn btn-primary"> Load More</button>
       </div>
     );
   }
